1d880c5a921a7b963b18f652ae392e495d94846a
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-02-15 14:12:37 +0100

    added dto for product registration

---
+++ src/main/kotlin/no/nav/hm/grunndata/dto/ProductRegistrationDTO.kt
@@ -0,0 +1,42 @@
+package no.nav.hm.grunndata.dto
+
+import java.time.LocalDateTime
+import java.util.*
+
+data class ProductRegistrationDTO(
+    val id: UUID = UUID.randomUUID(),
+    val supplierId: UUID,
+    val supplierRef: String,
+    val HMSArtNr: String?,
+    val title: String,
+    val draftStatus: DraftStatus = DraftStatus.DRAFT,
+    val adminStatus: AdminStatus = AdminStatus.NOT_APPROVED,
+    val status: RegistrationStatus = RegistrationStatus.ACTIVE,
+    val message: String?,
+    val adminInfo: AdminInfo?=null,
+    val created: LocalDateTime = LocalDateTime.now(),
+    val updated: LocalDateTime = LocalDateTime.now(),
+    val published: LocalDateTime?,
+    val expired: LocalDateTime?,
+    val updatedByUser: String="system",
+    val createdByUser: String="system",
+    val createdBy: String,
+    val updatedBy: String,
+    val createdByAdmin: Boolean = false,
+    val productDTO: ProductDTO,
+    val version: Long?=null
+)
+
+enum class RegistrationStatus {
+    ACTIVE, DELETED
+}
+
+enum class AdminStatus {
+    NOT_APPROVED, APPROVED
+}
+
+enum class DraftStatus {
+    DRAFT, DONE
+}
+
+data class AdminInfo(val approvedBy: String?, val note: String?=null)
