433ec990494dfca8c19331679b55ef6c66948aab
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-02-16 11:46:49 +0100

    added new event registerProductSync

--- src/main/kotlin/no/nav/hm/grunndata/rapid/event/EventName.kt
+++ src/main/kotlin/no/nav/hm/grunndata/rapid/event/EventName.kt
@@ -3,10 +3,11 @@ package no.nav.hm.grunndata.rapid.event
 class EventName {
 
     companion object {
-        const val hmdbagreementsync = "${RapidApp.grunndata_db}-hmdb-agreement-sync"
+        const val hmdbagreementsync = "${RapidApp.grunndata_db}-hmdb-agreement-sync" // send when sync from HMDB
         const val hmdbsuppliersync = "${RapidApp.grunndata_db}-hmdb-supplier-sync"
         const val hmdbproductsync = "${RapidApp.grunndata_db}-hmdb-product-sync"
-        const val productRegistration = "${RapidApp.grunndata_register}-product-registration"
+        const val productRegistration = "${RapidApp.grunndata_register}-product-registration" //send when product change in register
+        const val registerProductSync = "${RapidApp.grunndata_db}-product-registration-sync" // send from GDB when synced with register
     }
 
 }
