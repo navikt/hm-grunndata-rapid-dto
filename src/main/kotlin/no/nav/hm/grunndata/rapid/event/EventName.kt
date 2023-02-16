8e14269582f5e0d61189932fed7f9759b9856033
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-02-16 08:43:17 +0100

    added hm-grunndata-register event

--- src/main/kotlin/no/nav/hm/grunndata/rapid/event/EventName.kt
+++ src/main/kotlin/no/nav/hm/grunndata/rapid/event/EventName.kt
@@ -6,6 +6,7 @@ class EventName {
         const val hmdbagreementsync = "${RapidApp.grunndata_db}-hmdb-agreement-sync"
         const val hmdbsuppliersync = "${RapidApp.grunndata_db}-hmdb-supplier-sync"
         const val hmdbproductsync = "${RapidApp.grunndata_db}-hmdb-product-sync"
+        const val productRegistration = "${RapidApp.grunndata_register}-product-registration"
     }
 
 }
