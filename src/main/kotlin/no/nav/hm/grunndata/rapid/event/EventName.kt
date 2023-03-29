2e73d2fc26fc93255274ccc663c0a714ecaa8162
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-03-29 11:22:56 +0200

    put version into eventName

--- src/main/kotlin/no/nav/hm/grunndata/rapid/event/EventName.kt
+++ src/main/kotlin/no/nav/hm/grunndata/rapid/event/EventName.kt
@@ -7,12 +7,12 @@ package no.nav.hm.grunndata.rapid.event
 class EventName {
 
     companion object {
-        const val hmdbagreementsync = "${RapidApp.grunndata_db}-hmdb-agreement-sync" // send by db when sync from HMDB
-        const val hmdbsuppliersync = "${RapidApp.grunndata_db}-hmdb-supplier-sync" // send by db when sync from HMDB
-        const val hmdbproductsync = "${RapidApp.grunndata_db}-hmdb-product-sync" // send by db when sync from HMDB
-        const val syncedRegisterProduct  = "${RapidApp.grunndata_db}-register-product-synced" // send by GDB when synced with register
-        const val registeredProduct = "${RapidApp.grunndata_register}-registered-product" // send by REGISTER when product change in register
-        const val importedProduct = "${RapidApp.grunndata_import}-imported-product" // send by IMPORT when product got imported
+        const val hmdbagreementsyncV1 = "${RapidApp.grunndata_db}-hmdb-agreement-sync-v1" // send by db when sync from HMDB
+        const val hmdbsuppliersyncV1 = "${RapidApp.grunndata_db}-hmdb-supplier-sync-v1" // send by db when sync from HMDB
+        const val hmdbproductsyncV1 = "${RapidApp.grunndata_db}-hmdb-product-sync-v1" // send by db when sync from HMDB
+        const val syncedRegisterProductV1  = "${RapidApp.grunndata_db}-register-product-synced-v1" // send by GDB when synced with register
+        const val registeredProductV1 = "${RapidApp.grunndata_register}-registered-product-v1" // send by REGISTER when product change in register
+        const val importedProductV1 = "${RapidApp.grunndata_import}-imported-product-v1" // send by IMPORT when product got imported
     }
 
 }
