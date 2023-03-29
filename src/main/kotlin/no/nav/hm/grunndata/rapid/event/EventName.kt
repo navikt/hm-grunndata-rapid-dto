c344378054732c399dea465e4815cedcaa5b3095
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-03-29 11:12:55 +0200

    refactor event names, added imported product event

--- src/main/kotlin/no/nav/hm/grunndata/rapid/event/EventName.kt
+++ src/main/kotlin/no/nav/hm/grunndata/rapid/event/EventName.kt
@@ -1,13 +1,18 @@
 package no.nav.hm.grunndata.rapid.event
 
+/*
+    Events that floats in the rapid, this make it easier to know what to listen for
+    Eventname always start with app name (the source) for the event.
+ */
 class EventName {
 
     companion object {
-        const val hmdbagreementsync = "${RapidApp.grunndata_db}-hmdb-agreement-sync" // send when sync from HMDB
-        const val hmdbsuppliersync = "${RapidApp.grunndata_db}-hmdb-supplier-sync"
-        const val hmdbproductsync = "${RapidApp.grunndata_db}-hmdb-product-sync"
-        const val productRegistration = "${RapidApp.grunndata_register}-product-registration" //send when product change in register
-        const val registerProductSync = "${RapidApp.grunndata_db}-product-registration-sync" // send from GDB when synced with register
+        const val hmdbagreementsync = "${RapidApp.grunndata_db}-hmdb-agreement-sync" // send by db when sync from HMDB
+        const val hmdbsuppliersync = "${RapidApp.grunndata_db}-hmdb-supplier-sync" // send by db when sync from HMDB
+        const val hmdbproductsync = "${RapidApp.grunndata_db}-hmdb-product-sync" // send by db when sync from HMDB
+        const val syncedRegisterProduct  = "${RapidApp.grunndata_db}-register-product-synced" // send by GDB when synced with register
+        const val registeredProduct = "${RapidApp.grunndata_register}-registered-product" // send by REGISTER when product change in register
+        const val importedProduct = "${RapidApp.grunndata_import}-imported-product" // send by IMPORT when product got imported
     }
 
 }
--- src/main/kotlin/no/nav/hm/grunndata/rapid/event/RapidApp.kt
+++ src/main/kotlin/no/nav/hm/grunndata/rapid/event/RapidApp.kt
@@ -4,6 +4,7 @@ class RapidApp {
     companion object {
         const val grunndata_db = "hm-grunndata-db"
         const val grunndata_register="hm-grunndata-register"
+        const val grunndata_import = "hm-grunndata-import"
         const val grunndata_index="hm-grunndata-index"
         const val grunndata_media="hm-grunndata-media"
     }
