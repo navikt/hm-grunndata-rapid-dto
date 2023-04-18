633f48b9644950f16c38c93985fe830952293539
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-04-18 09:54:45 +0200

    refactor isocategory, not need for long texts

--- src/main/kotlin/no/nav/hm/grunndata/rapid/dto/IsoCategoryDTO.kt
+++ src/main/kotlin/no/nav/hm/grunndata/rapid/dto/IsoCategoryDTO.kt
@@ -2,19 +2,16 @@ package no.nav.hm.grunndata.rapid.dto
 
 import java.time.LocalDateTime
 
-data class IsoTranslations(
+data class IsoTranslationsDTO(
     val titleEn: String?=null,
-    val textLongEn: String?=null,
-    val textShortEn: String?=null
+    val textEn: String?=null,
 )
 
 data class IsoCategoryDTO(
     val isoCode: String,
     val isoTitle: String,
     val isoText: String,
-    val isoTextShort: String,
-    val isoTextLong: String,
-    val isoTranslations: IsoTranslations ? =null,
+    val isoTranslations: IsoTranslationsDTO ? =null,
     val isoLevel: Int,
     val isActive: Boolean = true,
     val showTech: Boolean = true,
