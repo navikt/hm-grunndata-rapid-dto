b568dee11a835d962cef545a3b46cbb5951a67f7
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-07-25 10:18:35 +0200

    make mediaDTO as a rapidDTO

--- src/main/kotlin/no/nav/hm/grunndata/rapid/dto/MediaDTO.kt
+++ src/main/kotlin/no/nav/hm/grunndata/rapid/dto/MediaDTO.kt
@@ -2,16 +2,16 @@ package no.nav.hm.grunndata.rapid.dto
 
 import java.util.*
 
-
-data class MediaDTO (
-    val oid: UUID = UUID.randomUUID(),
+data class MediaRapidDTO (
+    override val id: UUID,
+    val oid: UUID,
     val sourceUri: String,
     val uri:    String,
     val priority: Int = -1,
     val type: MediaType = MediaType.IMAGE,
     val text:   String?=null,
     val source: MediaSourceType = MediaSourceType.HMDB
-)
+):RapidDTO
 
 data class MediaInfo (
     val sourceUri: String,
