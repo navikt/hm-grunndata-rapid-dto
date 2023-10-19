38e47cd59fcb2666a510eaf9246d8873d2d46f6a
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-10-19 14:03:50 +0200

    added new rapid event dto for series registration

---
+++ src/main/kotlin/no/nav/hm/grunndata/rapid/dto/SeriesRegistrationRapidDTO.kt
@@ -0,0 +1,22 @@
+package no.nav.hm.grunndata.rapid.dto
+
+import java.time.LocalDateTime
+import java.util.*
+
+data class SeriesRegistrationRapidDTO(
+    override val id: UUID,
+    val draftStatus: DraftStatus = DraftStatus.DRAFT,
+    val supplierId:UUID,
+    val identifier: String,
+    val title: String,
+    val status: SeriesStatus = SeriesStatus.ACTIVE,
+    val created: LocalDateTime = LocalDateTime.now(),
+    val updated: LocalDateTime = LocalDateTime.now(),
+    val expired: LocalDateTime = LocalDateTime.now().plusYears(15),
+    val createdBy: String,
+    val updatedBy: String,
+    val updatedByUser: String,
+    val createdByUser: String,
+    val createdByAdmin: Boolean = false,
+    val version: Long? = 0L
+): RapidDTO
