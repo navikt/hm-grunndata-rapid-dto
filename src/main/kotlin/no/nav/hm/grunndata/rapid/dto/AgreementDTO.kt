aa4c0279f622c328fd8de36a18c8146b98f0d69f
Aasmund Nordstoga <aasmund.nordstoga@nav.no>
2024-02-07 09:56:08 +0100

    id on attachment is optional to start with

--- src/main/kotlin/no/nav/hm/grunndata/rapid/dto/AgreementDTO.kt
+++ src/main/kotlin/no/nav/hm/grunndata/rapid/dto/AgreementDTO.kt
@@ -32,7 +32,7 @@ data class AgreementPost(
 )
 
 data class AgreementAttachment(
-    val id: UUID,
+    val id: UUID?,
     val title: String?,
     val media: List<MediaInfo> = emptyList(),
     val description: String?
