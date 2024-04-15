19e3bbb586986c679a47d8d576b159af7b033dc4
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-04-15 11:50:46 +0200

    product agreement test

--- src/test/kotlin/no/nav/hn/grunndata/rapid/dto/ProductAgreementRegistrationDTOTest.kt
+++ src/test/kotlin/no/nav/hn/grunndata/rapid/dto/ProductAgreementRegistrationDTOTest.kt
@@ -1,5 +1,7 @@
 package no.nav.hn.grunndata.rapid.dto
 
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
 import no.nav.hm.grunndata.rapid.dto.ProductAgreementRegistrationRapidDTO
 import no.nav.hm.grunndata.rapid.dto.ProductAgreementStatus
 import org.junit.jupiter.api.Test
@@ -8,26 +10,26 @@ import java.time.LocalDateTime
 import java.util.*
 
 class ProductAgreementRegistrationDTOTest {
-
     @Test
     fun productAgreementRegistrationTest() {
+        val uuid = UUID.fromString("a03629d9-0c33-48b8-b997-fe940a153718")
         val productAgreementRapidDTO = ProductAgreementRegistrationRapidDTO(
-            id = UUID.randomUUID(),
-            productId = UUID.randomUUID(),
+            id = uuid,
+            productId = uuid,
             title = "beskrivelse av produktet",
-            supplierId = UUID.randomUUID(),
+            supplierId = uuid,
             supplierRef = "eksternref-1",
             hmsArtNr = "111",
-            agreementId = UUID.randomUUID(),
+            agreementId = uuid,
             reference = "AV-142",
             post = 1,
             rank = 1,
             status = ProductAgreementStatus.ACTIVE,
             createdBy = "REGISTER",
-            created = LocalDateTime.now(),
-            updated = LocalDateTime.now(),
-            published = LocalDateTime.now(),
-            expired = LocalDateTime.now().plusYears(4)
+            created = LocalDateTime.MAX,
+            updated = LocalDateTime.MAX,
+            published = LocalDateTime.MAX,
+            expired = LocalDateTime.MAX
         )
 
         //Serialize the object to a file
@@ -40,5 +42,7 @@ class ProductAgreementRegistrationDTOTest {
             this::class.java.getResourceAsStream("/old/ProductAgreementRegistration.json"),
             ProductAgreementRegistrationRapidDTO::class.java
         )
+        oldProductAgreementRegistration.shouldNotBeNull()
+        oldProductAgreementRegistration.post shouldBe 1
     }
 }
