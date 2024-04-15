fa423d0c5a006fb46d867e1c9045c82f929f0eb4
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-04-15 11:57:51 +0200

    series registration test

--- src/test/kotlin/no/nav/hn/grunndata/rapid/dto/SeriesRegistrationDTOTest.kt
+++ src/test/kotlin/no/nav/hn/grunndata/rapid/dto/SeriesRegistrationDTOTest.kt
@@ -1,7 +1,8 @@
 package no.nav.hn.grunndata.rapid.dto
 
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
 import no.nav.hm.grunndata.rapid.dto.AdminStatus
-import no.nav.hm.grunndata.rapid.dto.AgreementRegistrationRapidDTO
 import no.nav.hm.grunndata.rapid.dto.DraftStatus
 import no.nav.hm.grunndata.rapid.dto.MediaInfo
 import no.nav.hm.grunndata.rapid.dto.MediaSourceType
@@ -17,21 +18,21 @@ class SeriesRegistrationDTOTest {
 
     @Test
     fun testSeriesRegistrationDTO() {
-        val id = UUID.randomUUID()
+        val uuid = UUID.fromString("03f4f08f-36ec-4b7e-87ca-5542612a7ee5")
         val seriesRegistration = SeriesRegistrationRapidDTO(
-            id = id,
-            supplierId = supplier.id,
+            id = uuid,
+            supplierId = uuid,
             identifier = "HMDB-123",
             title = "Manuelle Rullestoler",
             text = "En lang beskrivelse av serien",
             isoCategory = "12001314",
             createdBy = "HMDB",
             updatedBy = "HMDB",
-            created = LocalDateTime.now(),
-            updated = LocalDateTime.now(),
+            created = LocalDateTime.MAX,
+            updated = LocalDateTime.MAX,
             status = SeriesStatus.ACTIVE,
             adminStatus = AdminStatus.APPROVED,
-            expired = LocalDateTime.now().plusYears(2),
+            expired = LocalDateTime.MAX,
             draftStatus = DraftStatus.DONE,
             count = 1,
             version = 1,
@@ -55,9 +56,12 @@ class SeriesRegistrationDTOTest {
         }
 
         //Deserialize old version if it works with this version
-        val oldAgreementRegistration = objectMapper().readValue(
+        val oldSeriesRegistration = objectMapper().readValue(
             this::class.java.getResourceAsStream("/old/SeriesRegistration.json"),
             SeriesRegistrationRapidDTO::class.java
         )
+        oldSeriesRegistration.shouldNotBeNull()
+        oldSeriesRegistration.title shouldBe "Manuelle Rullestoler"
+
     }
 }
