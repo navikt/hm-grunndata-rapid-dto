c5b7bb49d8634d3058648ca4186375626d6de908
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2024-08-15 14:51:24 +0200

    fix typo

--- src/main/kotlin/no/nav/hm/grunndata/rapid/dto/AgreementDTO.kt
+++ src/main/kotlin/no/nav/hm/grunndata/rapid/dto/AgreementDTO.kt
@@ -27,7 +27,7 @@ data class AgreementDTO(
 data class AgreementPost(
     val identifier: String,
     val id: UUID? = UUID.randomUUID(),
-    val type: DelkonktraktType = DelkonktraktType.WITH_DELKONTRAKT,
+    val type: DelkontraktType = DelkontraktType.WITH_DELKONTRAKT,
     val nr: Int,
     val refNr: String? = null,
     val title: String,
@@ -35,7 +35,7 @@ data class AgreementPost(
     val created: LocalDateTime = LocalDateTime.now()
 )
 
-enum class DelkonktraktType {
+enum class DelkontraktType {
     WITH_DELKONTRAKT, WITH_NO_DELKONTRAKT
 }
 
