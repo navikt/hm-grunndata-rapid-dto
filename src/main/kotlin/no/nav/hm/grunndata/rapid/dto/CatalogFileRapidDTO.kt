559a7c0f22258828e2d109b4aeb1a8e91d719922
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2025-01-21 17:19:09 +0100

    added a another state for catalogfile status

--- src/main/kotlin/no/nav/hm/grunndata/rapid/dto/CatalogFileRapidDTO.kt
+++ src/main/kotlin/no/nav/hm/grunndata/rapid/dto/CatalogFileRapidDTO.kt
@@ -17,5 +17,5 @@ data class CatalogFileRapidDTO(
 ): RapidDTO
 
 enum class CatalogFileStatus {
-    PENDING, DONE, ERROR
+    PENDING, DONE, ERROR, FINISHED // all accessories in catalog has been processed
 }
