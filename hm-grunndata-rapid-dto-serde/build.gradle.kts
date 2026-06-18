4d527de3e0d3ac0e7b6d3fcda1b7cab43625fc6e
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2026-06-18 08:26:09 +0200

    upgrade to micronaut 5.0, java 25, kotlin 2.3.0, and jackson 3.1

--- hm-grunndata-rapid-dto-serde/build.gradle.kts
+++ hm-grunndata-rapid-dto-serde/build.gradle.kts
@@ -1,8 +1,8 @@
-val micronautVersion="4.10.12"
+val micronautVersion="5.0.2"
 
 plugins {
 
-    id("io.micronaut.library") version "4.6.2"
+    id("io.micronaut.library") version "5.0.0"
 }
 
 dependencies {
